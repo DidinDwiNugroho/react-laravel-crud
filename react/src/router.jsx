14e0f871e8d64da838bb6c29f79e5fadef08a3c2
Didin Nugroho <didinnugroho69@gmail.com>
2023-10-25 11:30:35 +0700

    first commit

---
+++ react/src/router.jsx
@@ -0,0 +1,58 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Signup from "./views/Signup";
+import Login from "./views/Login";
+import Users from "./views/Users";
+import DefaultLayout from "./components/DefaultLayout";
+import Guestlayout from "./components/Guestlayout";
+import NotFound from "./views/NotFound";
+import Dashboard from "./views/Dashboard";
+import UserForm from "./views/UserForm";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/users" />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+      {
+        path: "/users/new",
+        element: <UserForm key="userCreate" />,
+      },
+      {
+        path: "/users/:id",
+        element: <UserForm key="userUpdate" />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Guestlayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
